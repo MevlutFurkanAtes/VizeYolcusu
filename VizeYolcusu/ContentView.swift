67c414d5f79cc443b1ea5dec150ad224a766d55b
Mevlüt Furkan Ateş <mevlutfurkanates@gmail.com>
2026-04-24 19:08:42 +0300

    Initial Commit

---
+++ VizeYolcusu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VizeYolcusu
+//
+//  Created by Mevlüt Furkan Ateş on 24.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VizeYolcusu/VizeYolcusuApp.swift
@@ -0,0 +1,17 @@
+//
+//  VizeYolcusuApp.swift
+//  VizeYolcusu
+//
+//  Created by Mevlüt Furkan Ateş on 24.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct VizeYolcusuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
